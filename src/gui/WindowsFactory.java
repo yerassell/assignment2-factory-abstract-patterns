0f7387b410cfe1ca9fa2e0f30027d7d845ccfbe3
yerassell <250577@astanait.edu.kz>
2026-09-17 13:01:08 +0500

    Create WindowsFactory.java

---
+++ src/gui/WindowsFactory.java
@@ -0,0 +1,10 @@
+package gui;
+
+public class WindowsFactory implements GUIFactory {
+    public Button createButton(){
+        return new WindowsButton();
+    }
+    public Checkbox createCheckbox(){
+        return new WindowsCheckbox();
+    }
+}
