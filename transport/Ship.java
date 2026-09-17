713b323a7563d128b659bf92b095593d1954cc00
yerassell <250577@astanait.edu.kz>
2026-09-17 12:34:02 +0500

    Create Ship.java

---
+++ transport/Ship.java
@@ -0,0 +1,8 @@
+package transport;
+
+public class Ship  implements Transport {
+    public void deliver() {
+        System.out.println("Ship delivers laboratory equipment to Aktau warehouse");
+    }
+}
+
