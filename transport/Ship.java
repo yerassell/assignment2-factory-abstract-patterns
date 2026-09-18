5f3eb78dadff2dbf2f2b115038a39cb5e4c996d1
yerassell <250577@astanait.edu.kz>
2026-09-18 22:15:21 +0500

    Update Ship.java

--- transport/Ship.java
+++ transport/Ship.java
@@ -1,8 +1,8 @@
 package transport;
 
 public class Ship  implements Transport {
-    public void deliver() {
-        System.out.println("Ship delivers laboratory equipment to Aktau warehouse");
+    public void deliver(String cargo, String destination) {
+        System.out.println("Ship delivers " + cargo + " to " + destination);
     }
 }
 
