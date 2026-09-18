5a2c131a1a6603419b33535a7d27e810282ea2d7
yerassell <250577@astanait.edu.kz>
2026-09-18 22:14:49 +0500

    Update Truck.java

--- transport/Truck.java
+++ transport/Truck.java
@@ -1,7 +1,7 @@
 package transport;
 
 public class Truck implements Transport {
-    public void deliver() {
-        System.out.println("Truck delivers laboratory equipment to Aktau warehouse");
+    public void deliver(String cargo, String destination) {
+        System.out.println("Truck delivers" + cargo + " to " + desination);
     }
 }
